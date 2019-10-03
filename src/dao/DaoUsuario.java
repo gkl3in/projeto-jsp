504ff205180e8a05d88023bb5ecf39a1b20e7a4c
Gabriel Klein <gabrielklein289@hotmail.com>
2019-10-03 18:15:05 -0300

    adicionado o ID e mais algumas alterações.

--- src/dao/DaoUsuario.java
+++ src/dao/DaoUsuario.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.corba.se.spi.orbutil.fsm.State;
+
 import beans.BeanCursoJsp;
 import connection.SingleConnection;
 
@@ -50,8 +52,66 @@ public class DaoUsuario {
 			BeanCursoJsp usuario = new BeanCursoJsp();
 			usuario.setLogin(resultado.getString(1));
 			usuario.setSenha(resultado.getString(2));
+			usuario.setId(resultado.getLong(3));
 			listarUsuarios.add(usuario);
 		}
 		return listarUsuarios;
+	}
+	
+	public void delete (String login) {
+		
+		try {
+			String sql = "delete from usuario where login = '" + login + "'";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.execute();
+			
+			connection.commit();
+		} catch(Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public BeanCursoJsp consultar(String login) throws SQLException {
+		
+		String sql = "select * from usuario where login='" + login + "'";
+		
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		if (resultSet.next()) {
+			BeanCursoJsp beanCursoJsp = new BeanCursoJsp();
+			beanCursoJsp.setLogin(resultSet.getString("login"));
+			beanCursoJsp.setSenha(resultSet.getString("senha"));
+			beanCursoJsp.setId(resultSet.getLong("id"));
+			
+			return beanCursoJsp;
+		}
+		
+		return null;
+	}
+
+	public void atualizar(BeanCursoJsp usuario) {
+		try {
+			String sql = "update usuario set login = ?, senha = ? where id = " + usuario.getId();
+			
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, usuario.getLogin());
+			statement.setString(2, usuario.getSenha());
+			statement.executeUpdate();
+			connection.commit();
+		} catch(Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
 	}
 }
